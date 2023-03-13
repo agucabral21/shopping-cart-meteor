c264cc85d522e0391605e44c66ef6b71dd0b594d
agus <agucabral@gmail.com>
2023-03-13 00:00:56 -0300

    refactor : created ClientView component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,38 +1,11 @@
-import React, { useState } from "react";
-import CartModal from "./CartModal";
-import Header from "./Header";
-import ProductGrid from "./ProductGrid";
+import React from "react";
+import { ClientView } from "./ClientView";
+import "../../imports/api/cart/cartMethods";
 
 export const App = () => {
-  const [showCartModal, setShowCartModal] = useState(false);
-  const [cart, setCart] = useState({ products: [] });
-
-  const cartProductsCount = cart.products.reduce((accumulator, product) => {
-    return accumulator + product.quantity;
-  }, 0);
-
-  const showCart = () => {
-    setShowCartModal(true);
-  };
-  const closeCart = () => {
-    setShowCartModal(false);
-  };
-
-  const addProductToCart = (product) => {
-    setCart((prevCart) => {
-      const updatedProducts = [...prevCart.products, product];
-      return { ...prevCart, products: updatedProducts };
-    });
-  };
   return (
     <div>
-      <Header onCartClick={showCart} cartProductsCount={cartProductsCount} />
-      <ProductGrid onAddProduct={addProductToCart} />
-      <CartModal
-        open={showCartModal}
-        closeModal={closeCart}
-        products={cart.products}
-      />
+      <ClientView />
     </div>
   );
 };
---
+++ imports/ui/ClientView.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import CartModal from "./CartModal";
+import Header from "./Header";
+import ProductGrid from "./ProductGrid";
+
+export const ClientView = () => {
+  const [showCartModal, setShowCartModal] = useState(false);
+  const [cart, setCart] = useState({ products: [] });
+
+  const cartProductsCount = cart.products.reduce((accumulator, product) => {
+    return accumulator + product.quantity;
+  }, 0);
+
+  const showCart = () => {
+    setShowCartModal(true);
+  };
+  const closeCart = () => {
+    setShowCartModal(false);
+  };
+
+  const addProductToCart = (product) => {
+    setCart((prevCart) => {
+      const updatedProducts = [...prevCart.products, product];
+      return { ...prevCart, products: updatedProducts };
+    });
+  };
+  return (
+    <div>
+      <Header onCartClick={showCart} cartProductsCount={cartProductsCount} />
+      <ProductGrid onAddProduct={addProductToCart} />
+      <CartModal
+        open={showCartModal}
+        closeModal={closeCart}
+        products={cart.products}
+      />
+    </div>
+  );
+};
