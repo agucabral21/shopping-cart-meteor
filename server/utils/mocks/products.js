3bcb38a87552b75a6c66e5b39e488e8863bacf9f
agus <agucabral@gmail.com>
2023-03-16 17:29:36 -0300

    mocks : updated mocks data

--- server/utils/mocks/products.js
+++ server/utils/mocks/products.js
@@ -1,114 +1,110 @@
 export default [
   {
-    name: "Shirt - 1",
+    name: "Flowers",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/B1kAAOSwihBgrg04/s-l500.png",
+    imageUrl: "https://i.postimg.cc/BvqvvB6J/04-2.png",
     stock: 30,
+    category: "Remeras",
   },
   {
-    name: "Shirt - 2",
+    name: "Dream",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/ydwAAOSwFKVgrg0w/s-l500.png",
+    imageUrl: "https://i.postimg.cc/DyX0pzH3/01.png",
     stock: 3,
+    category: "Remeras",
   },
   {
-    name: "Shirt - 3",
+    name: "Bonsai Turtle",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 11,
-    imageUrl: "https://i.ebayimg.com/images/g/UfwAAOSw~CNgrg00/s-l500.png",
+    imageUrl: "https://i.postimg.cc/V6hsDyFN/02.png",
     stock: 22,
+    category: "Remeras",
   },
   {
-    name: "Shirt - 4",
+    name: "Give us Pollen",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 1230,
-    imageUrl: "https://i.ebayimg.com/images/g/B1kAAOSwihBgrg04/s-l500.png",
+    imageUrl: "https://i.postimg.cc/xjkLYnht/03.png",
     stock: 1,
+    category: "Remeras",
   },
   {
-    name: "Shirt - 5",
+    name: "Make a Choice",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 32,
-    imageUrl: "https://i.ebayimg.com/images/g/zj0AAOSwDkBgrg09/s-l500.png",
+    imageUrl: "https://i.postimg.cc/g0N2LLX8/05-2.png",
     stock: 3,
+    category: "Remeras",
   },
   {
-    name: "Shirt - 6",
+    name: "Explore",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/tmoAAOSw-hBgrg1B/s-l500.png",
+    imageUrl: "https://i.postimg.cc/RFhV7CK8/06.png",
     stock: 3,
+    category: "Remeras",
   },
   {
-    name: "Shirt - 7",
+    name: "Blow It Up",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/J4MAAOSwysBgrg1F/s-l500.png",
+    imageUrl: "https://i.postimg.cc/d3TsfWWj/08.png",
     stock: 3,
+    category: "Remeras",
   },
   {
-    name: "Shirt - 8",
+    name: "Wild Heart",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/G1kAAOSwVzFgrg1K/s-l500.png",
+    imageUrl: "https://i.postimg.cc/nMbFfvgb/09.png",
     stock: 3,
+    category: "Remeras",
   },
   {
-    name: "Shirt - 9",
+    name: "Angel",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/G1kAAOSwVzFgrg1K/s-l500.png",
+    imageUrl: "https://i.postimg.cc/LXHn4Yfn/07.png",
     stock: 3,
+    category: "Remeras",
   },
   {
-    name: "Shirt - 10",
+    name: "Collage",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/G1kAAOSwVzFgrg1K/s-l500.png",
+    imageUrl: "https://i.postimg.cc/nrWz13WD/10.png",
     stock: 3,
+    category: "Remeras",
   },
   {
-    name: "Shirt - 11",
+    name: "Seeing Hand",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    price: 50,
+    imageUrl: "https://i.postimg.cc/qRjgNJXQ/12.png",
+    stock: 3,
+    category: "Remeras",
+  },
+  {
+    name: "Desert Blade",
     description:
       "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
     price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/G1kAAOSwVzFgrg1K/s-l500.png",
+    imageUrl: "https://i.postimg.cc/j222NpMT/11.png",
     stock: 3,
-  },
-  {
-    name: "Shirt - 12",
-    description:
-      "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
-    price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/ydwAAOSwFKVgrg0w/s-l500.png",
-    stock: 3,
-  },
-  {
-    name: "Shirt - 13",
-    description:
-      "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
-    price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/G1kAAOSwVzFgrg1K/s-l500.png",
-    stock: 3,
-  },
-  {
-    name: "Shirt - 14",
-    description:
-      "This is the description of the product, its a really detailed description that can help you to decide if you really like it.",
-    price: 50,
-    imageUrl: "https://i.ebayimg.com/images/g/G1kAAOSwVzFgrg1K/s-l500.png",
-    stock: 3,
+    category: "Remeras",
   },
 ];
